a943060f6b3ada6d83003400fb275cdc80ec7ec1
bilals2008 <wbiz2939@gmail.com>
2026-01-09 12:17:31 +0500

    add shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+// File: src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from "@/components/theme-provider";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
 
 // Use contextBridge
 window.ipcRenderer.on('main-process-message', (_event, message) => {
