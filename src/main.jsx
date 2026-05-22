e626582aac9a57db2b1948ba7d90081cac497467
bilals2008 <wbiz2939@gmail.com>
2026-05-22 11:58:17 +0500

    feat(theme): enforce dark-only mode, remove theme toggle and provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
+// File: src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from "@/components/theme-provider.jsx";
+
+document.documentElement.classList.add('dark')
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
